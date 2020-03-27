2d87d395471096050550224d06f53491d83e2659
Dmitriy <carnagery@mail.ru>
2020-03-28 00:11:48 +0400

    Commit 27.03.2020

---
+++ docs/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    nav: true,
+    margin: 10,
+    navText: [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>"
+    ]
+  });
+});
