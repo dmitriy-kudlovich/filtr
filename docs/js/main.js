6fd11aa92adcd19543ade79716693acc76d8c990
Dmitriy <carnagery@mail.ru>
2020-04-11 23:08:29 +0400

    Commit 11.04.2020

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     items: 3,
     nav: true,
     margin: 5,
     navText: [
       "<i class='fa fa-chevron-left'></i>",
-      "<i class='fa fa-chevron-right'></i>"
+      "<i class='fa fa-chevron-right'></i>",
     ],
     stagePadding: 50,
     responsiveClass: true,
@@ -13,12 +13,12 @@ $(document).ready(function() {
     dotsEach: 3,
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       480: {
-        items: 3
-      }
-    }
+        items: 3,
+      },
+    },
   });
 });
 
@@ -26,12 +26,12 @@ let hamburger = document.getElementById("hamburger");
 let navPanel = document.querySelector(".nav-panel");
 let body = document.querySelector("body");
 
-hamburger.onclick = function() {
+hamburger.onclick = function () {
   navPanel.classList.add("nav-panel--active");
   body.classList.add("overflow-hidden");
 };
 
-navPanel.onclick = function() {
+navPanel.onclick = function () {
   navPanel.classList.remove("nav-panel--active");
   body.classList.remove("overflow-hidden");
 };
